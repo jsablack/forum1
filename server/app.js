f510ce5555b3531cc03203734db05c6f929b60f0
Worn Jerabek Wiltse Architects <WJW@Worns-MacBook-Pro.local>
2017-03-17 09:54:28 -0500

    dotenv

--- server/app.js
+++ server/app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 
+var db = require('db')
+
+db.connect({
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS
+});
+
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
